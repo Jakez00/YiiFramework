b5ed8c95b93ab5291ee82b99a2eb948dd65700fe
Jake Getil <jakegetil@gmail.com>
2024-01-17 21:54:24 +0800

    author

--- views/site/articles.php
+++ views/site/articles.php
@@ -15,14 +15,16 @@ $this->title = 'Article Management';
 
     <div class="body-content">
 
-        <div class="row">
+        <div class="row justify-content-center">
             <?php foreach ($articles as $article): ?>
-                <div class="col-lg-4 mb-3 border border-1 p-3 mx-2 border">
+                <div class="col-lg-3 mb-3 border border-1 p-3 mx-2 border">
                     <h2><?= $article->title ?></h2>
 
                     <p><?= $article->content ?></p>
-
-                    <!-- <p><?= $article->user_id ?></p> -->
+                    <div class="mt-5">
+                        <p class="mb-0"><?=$article->user[0]->fullname?></p>
+                        <p style="font-size:12px;">Author</p>
+                    </div>
                 </div>
             <?php endforeach; ?>
         </div>
